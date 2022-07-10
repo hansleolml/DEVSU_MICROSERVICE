1ce919810eed06a00b247f7df3a7c519e96dda63
unknown <hansleolml@gmail.com>
2022-07-09 20:19:07 -0500

    add unit test

---
+++ src/test/java/com/devsu/demo/expose/advice/ControllerAdviceTest.java
@@ -0,0 +1,10 @@
+package com.devsu.demo.expose.advice;
+
+import org.junit.jupiter.api.Test;
+
+public class ControllerAdviceTest {
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
